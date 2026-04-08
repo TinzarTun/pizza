84fc032a4c1561dfa1322ad4dd4eddf767ff4418
Tinzar Tun <tinzartun2002@gmail.com>
2026-04-08 23:58:56 +0630

    fix route position

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/register', [AuthController::class, 'getregister'])->name('register');
-Route::get('/login', [AuthController::class, 'getlogin'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::post('/register', [AuthController::class,'postregister'])->name('register.post');
-Route::post('/login', [AuthController::class,'postlogin'])->name('login.post');
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -23,3 +16,10 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/register', [AuthController::class, 'getregister'])->name('register');
+Route::get('/login', [AuthController::class, 'getlogin'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::post('/register', [AuthController::class,'postregister'])->name('register.post');
+Route::post('/login', [AuthController::class,'postlogin'])->name('login.post');
