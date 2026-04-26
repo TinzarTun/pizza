a599d82a0738b4e9f7f3d6eb841c1ed99477dbf8
Tinzar Tun <tinzartun2002@gmail.com>
2026-04-26 23:30:20 +0630

    create LoginResponse

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+use Illuminate\Support\Facades\Auth;
+
+class LoginResponse implements LoginResponseContract
+{
+    public function toResponse($request)
+    {
+        $user = Auth::user();
+
+        if ($user->role === 'admin') {
+            return redirect('/dashboard');
+        }
+
+        return redirect('/my/profile');
+    }
+}
