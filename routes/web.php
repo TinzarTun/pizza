4ed424c75a8b58e2d432c08ee1bd5d173643152b
Tinzar Tun <tinzartun2002@gmail.com>
2026-04-30 20:51:41 +0630

    declare User routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,19 @@ Route::group(['prefix'=> 'page'], function () {
     Route::get('/login', [AuthController::class, 'getlogin'])->name('page.login');
 });
 
+// User routes
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+
+    Route::get('/my/profile', function () {
+        return view('profile');
+    })->name('my.profile');
+
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
