a56eb5afd1d69be014b2a2272dd8660e1acf37a1
Tinzar Tun <tinzartun2002@gmail.com>
2026-05-02 15:59:49 +0630

    edit user middleware

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserMiddleware
@@ -15,6 +16,14 @@ class UserMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role !== 'user') {
+            abort(403);
+        }
+
         return $next($request);
     }
 }
